b27b59466ad03d9824850606c1380a514db2128d
Munawar Bijani <munawarb@gmail.com>
2018-11-14 11:20:50 -0500

    All projects point to the same AssemblyInfo.cs for common versioning

--- TDV/AssemblyInfo.cs
+++ TDV/AssemblyInfo.cs
@@ -27,19 +27,3 @@ using System.Runtime.InteropServices;
 
 //The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("00834477-B14F-4D8F-A51E-4FF2200D6CB3")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-#if BETA
-[assembly: AssemblyVersion("2.21.5.*")]
-#else
-[assembly: AssemblyVersion("2.21.0.*")]
-[assembly: AssemblyFileVersion("2.21.0.0")]
-#endif
--- Updater/Properties/AssemblyInfo.cs
+++ Updater/Properties/AssemblyInfo.cs
@@ -21,13 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("5eae509b-1001-4325-8b4c-62b3d4a0b1d2")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
