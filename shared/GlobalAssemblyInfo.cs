2eda6e8b1c12d09c4fd6ad9b7e61caa71cdacd3a
Munawar Bijani <munawarb@gmail.com>
2018-12-01 16:24:09 -0500

    Added GlobalAssemblyInfo.cs for common version numbering

---
+++ shared/GlobalAssemblyInfo.cs
@@ -0,0 +1,22 @@
+/* This source is provided under the GNU AGPLv3  license. You are free to modify and distribute this source and any containing work (such as sound files) provided that:
+* - You make available complete source code of modifications, even if the modifications are part of a larger project, and make the modified work available under the same license (GNU AGPLv3).
+* - You include all copyright and license notices on the modified source.
+* - You state which parts of this source were changed in your work
+* Note that containing works (such as SharpDX) may be available under a different license.
+* Copyright (C) Munawar Bijani
+*/
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("2.21")]
+[assembly: AssemblyFileVersion("2.21")]
